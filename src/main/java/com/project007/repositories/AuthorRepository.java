7ee93258a9882db56b8f0c22ccf5860caddfba24
Lordmauldeidad <freepmaul@gmail.com>
2026-02-05 11:53:52 -0500

    faltaba @inject

--- src/main/java/com/project007/repositories/AuthorRepository.java
+++ src/main/java/com/project007/repositories/AuthorRepository.java
@@ -10,7 +10,7 @@ import jakarta.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
 public class AuthorRepository extends GenericRepoImpl<Author, String>{
-
+    @Inject
     public AuthorRepository(ExecutorService executor, GraphTemplate graphTemplate, Class<?> clazz) {
         super(executor, graphTemplate, Author.class);
     }
